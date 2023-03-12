0d51807c0e384629cb02a4d9170290aa9ec8bb11
Bhimgouda <satvikp508@gmail.com>
2023-03-12 10:57:24 +0530

    inheritance and interface added

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -15,9 +15,15 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) internal {
         zombies.push(Zombie(_name, _dna));
-        emit NewZombie(zombies.length-1,_name, _dna);
+        uint id = zombies.length-1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
@@ -26,8 +32,11 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
 }
+
