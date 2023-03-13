792bf9fe09bd2b7d00b97b788e8b604b974297f0
Bhimgouda <satvikp508@gmail.com>
2023-03-13 16:16:04 +0530

    Tokenized zombies as NFT with ERC-721

---
+++ erc721.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+// This is an abstract contract
+// It does not contain any implementation details and is intended to be used as an interface only.
+
+abstract contract ERC721 {
+  event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+  event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+
+  function balanceOf(address _owner) external virtual view returns (uint256);
+  function ownerOf(uint256 _tokenId) external virtual view returns (address);
+  function transferFrom(address _from, address _to, uint256 _tokenId) external virtual payable;
+  function approve(address _approved, uint256 _tokenId) external virtual payable;
+}
--- zombiehelper.sol
+++ zombiehelper.sol
@@ -12,18 +12,25 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
+  function withdraw() external onlyOwner {
+    address payable _owner = payable(owner());
+    _owner.transfer(address(this).balance);
+  }
+
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
+  }
+
   function levelUp(uint _zombieId) external payable {
     require(msg.value == levelUpFee);
     zombies[_zombieId].level++;
   }
 
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].name = _newName;
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) onlyOwnerOf(_zombieId) {
     zombies[_zombieId].dna = _newDna;
   }
 
