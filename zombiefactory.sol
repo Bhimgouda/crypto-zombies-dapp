ab7c3bb3c308967a94fc5e038e325374cf14324f
Bhimgouda <satvikp508@gmail.com>
2023-03-13 08:53:22 +0530

    now contract is payable

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -9,11 +9,13 @@ contract ZombieFactory is Ownable {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
-        string name;
-        uint dna;
-        // Add new data here
+      string name;
+      uint dna;
+      uint32 level;
+      uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -22,7 +24,7 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        zombies.push(Zombie(_name, _dna));
+        zombies.push(Zombie(_name, _dna, 1, uint32(block.timestamp + cooldownTime)));
         uint id = zombies.length-1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
@@ -42,5 +44,9 @@ contract ZombieFactory is Ownable {
     }
 
 }
+
+
+
+
 
 
